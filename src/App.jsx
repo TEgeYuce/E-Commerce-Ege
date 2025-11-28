89c08caa3e86c882f7d1b5b586012f088b5a379c
TEgeYuce <tegeyucesan@gmail.com>
2025-11-28 11:54:44 +0300

    "App.css", "App.jsx" ve "main.jsx" Dosyaları Tamamlandı

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Header from './layout/Header';
+import PageContent from './layout/PageContent';
+import Footer from './layout/Footer';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className='font-montserrat'>
+      <Header />
+      <PageContent />
+      <Footer />
+    </div>
+  )
+}
+
+export default App;
