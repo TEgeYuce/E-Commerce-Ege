02d178c98d07831dbd8690f9df40de189c7f1e91
TEgeYuce <tegeyucesan@gmail.com>
2025-11-28 12:10:35 +0300

    Ana Sayfaya (HomePage'e) ait Component'ler Oluşturulup Tamamlandı

---
+++ src/components/MarkaLogolar.jsx
@@ -0,0 +1,21 @@
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import { useLocation } from "react-router-dom";
+import {faLyft, faHooli, faStripe, faPiedPiperHat, faRedditAlien, faAws} from "@fortawesome/free-brands-svg-icons";
+
+export default function Clients() {
+    
+    const location = useLocation();
+
+    const bgColor = location.pathname == "/" ? "bg-white" : "bg-[#FAFAFA]";
+    
+    return (
+        <section className={`clients text-[#737373] flex flex-col md:flex-row items-center py-[3.125rem] md:px-[3.125rem] md:justify-center gap-[3.75rem] text-8xl ${bgColor}`}>
+            <FontAwesomeIcon icon={faHooli}/>
+            <FontAwesomeIcon icon={faLyft} />
+            <FontAwesomeIcon icon={faPiedPiperHat} />
+            <FontAwesomeIcon icon={faStripe} />
+            <FontAwesomeIcon icon={faAws} />
+            <FontAwesomeIcon icon={faRedditAlien} />
+        </section>
+    );
+}
