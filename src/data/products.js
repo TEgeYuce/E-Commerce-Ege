f15bab5424ee40e55a590c48c5f6b27ae49d7fab
TEgeYuce <tegeyucesan@gmail.com>
2025-12-07 22:00:15 +0300

    "ShopPage" İçin Gereken Ürünlere ait Bilgiler ve Resimler "data" Klasörü İçerisindeki "products.js" Dosyasında Oluşturuldu

---
+++ src/data/products.js
@@ -0,0 +1,164 @@
+export const products = [
+
+  {
+    "product_name": "Incredible Rubber Cheese",
+    "product_desc": "The Kelsie Car is the latest in a series of yellowish products from Walsh LLC",
+    "product_img": "https://d5e14a.a-cdn.akinoncloud.com/products/2025/03/11/1411159/3dd66f7b-3551-47ca-899d-a71df192f277_size900x1350.jpg",
+    "product_price": "915.15",
+    "product_discountPrice": "428.95",
+    "id": "1"
+  },
+  {
+    "product_name": "Handcrafted Concrete Salad",
+    "product_desc": "Discover the turtle-like agility of our Pizza, perfect for babyish users",
+    "product_img": "https://www.tarzkadin.com/wp-content/uploads/2021/05/Erkek-Giyim-Tarzi-Nasil-Olmali.jpg",
+    "product_price": "334.89",
+    "product_discountPrice": "767.79",
+    "id": "2"
+  },
+  {
+    "product_name": "Recycled Granite Ball",
+    "product_desc": "The Jewel Towels is the latest in a series of rigid products from Mohr - Barton",
+    "product_img": "https://static.ticimax.cloud/cdn-cgi/image/width=-,quality=85/30422/uploads/urunresimleri/buyuk/beyaz-gomlekli-sik-spor-kombin-f-44b3.jpg",
+    "product_price": "768.89",
+    "product_discountPrice": "579.35",
+    "id": "3"
+  },
+  {
+    "product_name": "Rustic Plastic Bike",
+    "product_desc": "Featuring Rutherfordium-enhanced technology, our Bacon offers unparalleled sudden performance",
+    "product_img": "https://sc04.alicdn.com/kf/H61250bf1671d4f06a39a5291936ecf426.jpg",
+    "product_price": "445.15",
+    "product_discountPrice": "763.79",
+    "id": "4"
+  },
+  {
+    "product_name": "Practical Plastic Shoes",
+    "product_desc": "Discover the cow-like agility of our Gloves, perfect for hateful users",
+    "product_img": "https://www.bandito.com.tr/image/cache/data/background/studyo_2024/galata/_0072_124_0507-366x506.jpg",
+    "product_price": "720.49",
+    "product_discountPrice": "698.59",
+    "id": "5"
+  },
+  {
+    "product_name": "Rustic Cotton Sausages",
+    "product_desc": "New Tuna model with 81 GB RAM, 6 GB storage, and normal features",
+    "product_img": "https://aydinli-pc.a-cdn.akinoncloud.com/cms/2022/08/01/f6a50aa0-9373-401f-86d8-26612cffd157.jpg",
+    "product_price": "137.79",
+    "product_discountPrice": "865.39",
+    "id": "6"
+  },
+  {
+    "product_name": "Oriental Metal Pants",
+    "product_desc": "Innovative Car featuring wretched technology and Granite construction",
+    "product_img": "https://www.bigdart.com.tr/t1627-yaka-bagcikli-elbise-tas-elbise-bigdart-18097721-32-O.jpg",
+    "product_price": "537.39",
+    "product_discountPrice": "355.79",
+    "id": "7"
+  },
+  {
+    "product_name": "Awesome Marble Cheese",
+    "product_desc": "Innovative Keyboard featuring young technology and Gold construction",
+    "product_img": "https://cdn.oxxo.com.tr/ContentImages/Product/26y/26YOX-MOPANTBAG1/modal-straight-fit-pantolon_silver-birch-krem_1_detay.jpg",
+    "product_price": "307.49",
+    "product_discountPrice": "820.25",
+    "id": "8"
+  },
+  {
+    "product_name": "Licensed Wooden Ball",
+    "product_desc": "Discover the neat new Tuna with an exciting mix of Marble ingredients",
+    "product_img": "https://i.pinimg.com/474x/55/ba/7d/55ba7d973f1fb6da74ab9584b44554eb.jpg",
+    "product_price": "54.65",
+    "product_discountPrice": "695.25",
+    "id": "9"
+  },
+  {
+    "product_name": "Luxurious Concrete Computer",
+    "product_desc": "Stylish Pizza designed to make you stand out with entire looks",
+    "product_img": "https://static.ticimax.cloud/cdn-cgi/image/width=540,quality=99,format=webp/4197/uploads/urunresimleri/buyuk/setreekru-100-pamuk-belden-serit-detay-4-36d0.jpg",
+    "product_price": "829.95",
+    "product_discountPrice": "418.19",
+    "id": "10"
+  },
+  {
+    "product_name": "Unbranded Bronze Gloves",
+    "product_desc": "New teal Gloves with ergonomic design for adolescent comfort",
+    "product_img": "https://ideacdn.net/idea/gz/84/myassets/blogs/blog-7.png?revision=1720165990",
+    "product_price": "707.69",
+    "product_discountPrice": "154.15",
+    "id": "11"
+  },
+  {
+    "product_name": "Elegant Silk Car",
+    "product_desc": "Ergonomic Chicken made with Silk for all-day fine support",
+    "product_img": "https://cansinmini.com/img/s/16/krem-citcitli-gomlekli-erkek-cocuk-3lu-tisortlu-takim-23380-76116.jpg",
+    "product_price": "775.99",
+    "product_discountPrice": "79.65",
+    "id": "12"
+  },
+  {
+    "product_name": "Incredible Steel Salad",
+    "product_desc": "Introducing the Benin-inspired Ball, blending fussy style with local craftsmanship",
+    "product_img": "https://static.ticimax.cloud/cdn-cgi/image/width=540,quality=85/10690/uploads/urunresimleri/buyuk/breeze-erkek-cocuk-uzun-kollu-tisort-a-35b5d2.jpg",
+    "product_price": "155.39",
+    "product_discountPrice": "640.85",
+    "id": "13"
+  },
+  {
+    "product_name": "Oriental Granite Keyboard",
+    "product_desc": "Professional-grade Cheese perfect for glum training and recreational use",
+    "product_img": "https://a4c976.a-cdn.akinoncloud.com/products/2025/10/06/310888/74a7474d-17bf-4dd4-b7cf-23375601e7ed_size3840x5760_cropCenter.jpg",
+    "product_price": "661.86",
+    "product_discountPrice": "641.69",
+    "id": "14"
+  },
+  {
+    "product_name": "Fresh Metal Table",
+    "product_desc": "New orchid Computer with ergonomic design for damp comfort",
+    "product_img": "https://static.ticimax.cloud/69976/uploads/urunresimleri/buyuk/erkek-cocuk-alt-ust-takim-23-nisan-29--02f1-9.jpg",
+    "product_price": "298.51",
+    "product_discountPrice": "410.59",
+    "id": "15"
+  },
+  {
+    "product_name": "Oriental Bamboo Soap",
+    "product_desc": "Ergonomic Computer made with Bronze for all-day utter support",
+    "product_img": "https://static.ticimax.cloud/cdn-cgi/image/width=540,quality=85/11513/uploads/urunresimleri/buyuk/burcak-sile-bezi-kiz-cocuk-elbise-yesi--e2fa-.jpg",
+    "product_price": "834.79",
+    "product_discountPrice": "902.85",
+    "id": "16"
+  },
+  {
+    "product_name": "Refined Silk Chips",
+    "product_desc": "The Julianne Shirt is the latest in a series of surprised products from Carter - Konopelski",
+    "product_img": "https://dfcdn.defacto.com.tr/7/D1087A8_25WN_GN1123_01_03.jpg",
+    "product_price": "788.85",
+    "product_discountPrice": "548.69",
+    "id": "17"
+  },
+  {
+    "product_name": "Luxurious Aluminum Towels",
+    "product_desc": "Featuring Molybdenum-enhanced technology, our Keyboard offers unparalleled tragic performance",
+    "product_img": "https://images3.farktorcdn.com/img/800x1200/Library/Upl/5500142/Product/8684384760372-336.jpg",
+    "product_price": "580.59",
+    "product_discountPrice": "368.99",
+    "id": "18"
+  },
+  {
+    "product_name": "Oriental Granite Fish",
+    "product_desc": "The sleek and everlasting Pizza comes with yellow LED lighting for smart functionality",
+    "product_img": "https://static.ticimax.cloud/30387/uploads/blog/kiz-cocuk-kiyafetleri-f326.jpg",
+    "product_price": "24.59",
+    "product_discountPrice": "369.99",
+    "id": "19"
+  },
+  {
+    "product_name": "Intelligent Ceramic Hat",
+    "product_desc": "Savor the tangy essence in our Table, designed for meager culinary adventures",
+    "product_img": "https://cdn.dsmcdn.com/mnresize/400/-/ty1720/prod/QC_ENRICHMENT/20250804/14/d5765f12-49cb-36df-ad1e-e38e1a2b865d/1_org_zoom.jpg",
+    "product_price": "801.19",
+    "product_discountPrice": "568.09",
+    "id": "20"
+  }
+
+];
