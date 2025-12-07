72c99c4947a5d7aa020942e3a34f949bc6f2f47e
TEgeYuce <tegeyucesan@gmail.com>
2025-12-07 22:24:14 +0300

    "ShopPage.jsx" Dosyası Component'lerle Birlikte Tamamlandı

--- src/pages/ShopPage.jsx
+++ src/pages/ShopPage.jsx
@@ -1,7 +1,50 @@
+import {NavLink} from "react-router-dom/cjs/react-router-dom.min";
+import MarkaLogolar from "../components/sharedcomp/MarkaLogolar";
+import ShopKategoriler from "../components/shopPage/ShopKategoriler";
+import ShopProductKart from "../components/shopPage/ShopProductKart";
+import { products } from "../data/products";
+
 const ShopPage = () => {
+
+
   return (
-    <div>       This page is: Shop Page</div>
-  )
-}
+    <div className="px-10 md:px-35 py-10">
+      <div className="shopheader flex justify-between">
+        <p className="text-2xl font-bold leading-8 text-[#252B42]">Shop</p>
+        <div className="flex gap-4">
+          <NavLink to="/">Home</NavLink>
+          <span className="text-bold"> / </span>
+          <p className="text-[#BDBDBD]">Shop</p>
+        </div>
+      </div>
+      <div className="shopCategories mt-10 ">
+        <ShopKategoriler />
+      </div>
+
+      <section className="py-16 px-4">
+      <div className="mb-6 flex justify-between items-center">
+        <p>Showing all {products.length} results</p>
+      </div>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+        {products.map(product => (
+          <ShopProductKart
+            id={product.id}
+            key={product.id}
+            product_name={product.product_name}
+            product_desc={product.product_desc}
+            product_price={product.product_price}
+            product_discountPrice={product.product_discountPrice}
+            product_img={product.product_img}
+          />
+        ))}
+      </div>
+
+    </section>
+    
+      <MarkaLogolar />
+    </div>
+  );
+};
 
 export default ShopPage;
