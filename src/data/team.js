8b7a434a87b8238c76bc0ec80b1144c85cdbd278
TEgeYuce <tegeyucesan@gmail.com>
2025-12-13 20:51:24 +0300

    "TeamPage" İçin Gereken Resimler ve Yazılar "data" Klasörü İçerisindeki "team.js" Dosyasında Oluşturuldu

---
+++ src/data/team.js
@@ -0,0 +1,27 @@
+export const team = [
+  {
+    name: "Tuncer Ege Yücesan",
+    role: "Full Stack Web Developer",
+    avatar:
+      "tegeyuce.png",
+    id: "1",
+  },
+  {
+    name: "Gökhan Özdemir",
+    role: "Project Manager",
+    avatar: "gokhanozdemir.jfif",
+    id: "2",
+  },
+  {
+    name: "Willie Ledner",
+    role: "Dynamic Response Planner",
+    avatar: "https://avatars.githubusercontent.com/u/28084390",
+    id: "3",
+  },
+  {
+    name: "Emma Walker",
+    role: "Central Metrics Orchestrator",
+    avatar: "https://avatars.githubusercontent.com/u/41992156",
+    id: "4",
+  },
+];
