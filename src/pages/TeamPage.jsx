c38d73389c6605cee13f46dc5e9c74649a8fce74
TEgeYuce <tegeyucesan@gmail.com>
2025-12-14 12:51:32 +0300

    "TeamPage" İçin Gereken "TakimUye.jsx" Component'i ve "TeamPage.jsx" Dosyası Oluşturulup Tamamlandı

---
+++ src/pages/TeamPage.jsx
@@ -0,0 +1,27 @@
+import TakimUye from "../components/teampage/TakimUye";
+import { team } from "../data/team";
+
+const TeamPage = () => {
+
+  return (
+   <section className="py-16 px-[112px] text-center">
+      <h2 className="text-3xl font-bold mb-4">Meet Our Team</h2>
+      <p className="mb-10 text-gray-500 max-w-xl mx-auto">
+        Problems trying to resolve the conflict between the two major realms of Classical physics: Newtonian mechanics
+      </p>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+        {team.map((member) => (
+          <TakimUye
+            key={member.id}
+            name={member.name}
+            role={member.role}
+            avatar={member.avatar}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default TeamPage;
